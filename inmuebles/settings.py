4fefd400ddfd1465c1e81a7863d31ce1508ca1ca
Sebastian Acosta <sebas.sas@gmail.com>
2024-03-11 18:22:35 -0500

    DEBUG correction

--- inmuebles/settings.py
+++ inmuebles/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.path.get("DEBUG", "False").lower() == "true"
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
 
